7e949c5e2ba2af281d063276577d8ed9bd969da2
Andrey Dyugay <andrey24dyugay@gmail.com>
2024-03-15 17:28:08 +0300

    animations for mc

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D playerBody;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    private float dirX = 0;
+    private float moveSpeed = 7f;
+    private float jumpSpeed = 14f;
+
+    private enum MovementState
+    {
+        idle,
+        running,
+        jumping,
+        falling
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        playerBody.velocity = new Vector2(dirX * moveSpeed, playerBody.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerBody.velocity = new Vector2(playerBody.velocity.x, jumpSpeed);
+        }
+
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        MovementState state;
+
+        if (dirX > 0f)
+        {
+            state = MovementState.running;
+            spriteRenderer.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            state = MovementState.running;
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            state = MovementState.idle;
+        }
+
+        if (playerBody.velocity.y > .01f)
+        {
+            state = MovementState.jumping;
+        } 
+        else if (playerBody.velocity.y < -.01f)
+        {
+            state = MovementState.falling;
+        }
+
+        animator.SetInteger("state", (int)state);
+    }
+}
